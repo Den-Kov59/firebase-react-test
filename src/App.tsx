569f6119f016ec47fe9b1cc62b0c8922adc1d5f0
Denis Kovalenko <den.kov.2015@gmail.com>
2024-11-13 18:36:51 +0200

    sda

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { getMessaging, onMessage, getToken } from "firebase/messaging";
 import io from 'socket.io-client';
