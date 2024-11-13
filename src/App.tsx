6e14c052656608c85985f480a9c9a4daf29de169
Denis Kovalenko <den.kov.2015@gmail.com>
2024-11-13 16:36:01 +0200

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { initializeApp } from "firebase/app";
+import { getMessaging, onMessage } from "firebase/messaging";
+import io from 'socket.io-client';
 
 const firebaseConfig = {
-  apiKey: "12",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "test-app-cbb2d.firebaseapp.com",
   projectId: "test-app-cbb2d",
-  storageBucket: "12",
+  storageBucket: "test-app-cbb2d.firebasestorage.app",
   messagingSenderId: "110679803978",
   appId: "1:110679803978:web:69f5c38b379a7255b5beb3"
 };
 
 
+
+
 function App() {
+  const [messages, setMessages] = useState(['']);
+  const [message, setMessage] = useState('');
+  
+  const socket = io('https://service-app-1-110679803978.europe-west8.run.app'); // Adjust the URL to your backend
+
+// Connect to the server
+socket.on('connect', () => {
+  console.log('Connected to WebSocket server');
+});
+
+
+const sendMessageToServer = (message: string) => {
+  socket.emit('message', message);
+};
   
   const app = initializeApp(firebaseConfig);
-  console.log(app);
+
+  const messaging = getMessaging(app);
+  
+  onMessage(messaging, (payload) => {
+    console.log('Message received. ', payload);
+    messages.push(payload.messageId);
+    setMessages(messages);
+  })
+  
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    sendMessageToServer(message);
+    setMessage('');
+  };
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Enter your message"
+      />
+      <button type="submit">Send</button>
+      </form>
+      <div>
+        {messages.map((mes) => {
+          return <p>{mes}</p>
+        })}
+      </div>
     </div>
   );
 }
