85739f2b3621bd68104812bc77b5e956bd52cb5a
Denis Kovalenko <den.kov.2015@gmail.com>
2024-11-14 18:36:05 +0200

    change config in worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,16 +5,14 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "test-app-cbb2d.firebaseapp.com",
-  projectId: "test-app-cbb2d",
-  storageBucket: "test-app-cbb2d.firebasestorage.app",
-  messagingSenderId: "110679803978",
-  appId: "1:110679803978:web:69f5c38b379a7255b5beb3"
+const defaultConfig = {
+  apiKey: true,
+  projectId: true,
+  messagingSenderId: true,
+  appId: true,
 };
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(defaultConfig);
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
